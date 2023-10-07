b298c9136c1bf30e2584622c580d6d2dd9a8119d
nichakamol <nichakamol_sanghong@cmu.ac.th>
2023-10-07 15:27:25 +0700

    commit sed laew yeah

---
+++ src/main/java/se331/lab/rest/controller/OrganizerController.java
@@ -0,0 +1,46 @@
+package se331.lab.rest.controller;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.server.ResponseStatusException;
+import se331.lab.rest.entity.Organizer;
+import se331.lab.rest.service.OrganizerService;
+
+import java.util.List;
+@Controller
+@RequiredArgsConstructor
+public class OrganizerController {
+    final OrganizerService organizerService;
+
+    @GetMapping("organizer")
+    public ResponseEntity<?> getOrganizerLists(@RequestParam(value = "_limit",required = false)Integer perPage, @RequestParam(value = "_page",required = false)Integer page){
+        List<Organizer> output = null;
+        Integer organizerSize = organizerService.getOrganizerSize();
+        HttpHeaders responseHeader = new HttpHeaders();
+        responseHeader.set("x-total-count",
+                String.valueOf(organizerSize));
+        try {
+            output = organizerService.getOrganizer(perPage, page);
+            return new
+                    ResponseEntity<>(output,responseHeader,HttpStatus.OK);
+        } catch (IndexOutOfBoundsException ex) {
+            return new
+                    ResponseEntity<>(output,responseHeader,HttpStatus.OK);
+        }
+    }
+    @GetMapping("organizer/{id}")
+    public  ResponseEntity<?>getOrganizer(@PathVariable("id") Long id) {
+        Organizer output = organizerService.getOrganizer(id);
+        if (output!= null) {
+            return ResponseEntity.ok(output);
+        }else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND,"The given id is not found");
+        }
+    }
+}
+
