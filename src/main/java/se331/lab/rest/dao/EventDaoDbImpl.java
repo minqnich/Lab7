c0f1874f0c637cd939e36716e43d9e10beffc4fb
nichakamol <nichakamol_sanghong@cmu.ac.th>
2023-10-07 20:53:27 +0700

    lab08

---
+++ src/main/java/se331/lab/rest/dao/EventDaoDbImpl.java
@@ -0,0 +1,30 @@
+package se331.lab.rest.dao;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import se331.lab.rest.entity.Event;
+import se331.lab.rest.repository.EventRepository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class EventDaoDblmpl implements EventDao{
+    final EventRepository eventRepository;
+    @Override
+    public Integer getEventSize() {
+        return Math.toIntExact(eventRepository.count());
+    }
+    @Override
+    public  List<Event> getEvents(Integer pageSize, Integer page) {
+        List<Event> events = eventRepository.findAll();
+        pageSize = pageSize == null ? events.size() : pageSize;
+        page = page == null ? 1 : page;
+        int firstIndex = (page - 1) * pageSize;
+        List<Event> output = events.subList(firstIndex, firstIndex + pageSize);
+        return output;
+    }
+    @Override
+    public EventRepository.findById(id).orElse(null);
+}
